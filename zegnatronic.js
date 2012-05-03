a3f3232a36d8e12801d4eec6b6cc73e273078a81
Samuel Merritt <spam@andcheese.org>
2012-05-02 17:15:10 -0700

    seems to be working

--- zegnatronic.js
+++ zegnatronic.js
@@ -5,8 +5,9 @@ var irc_nick = process.env.Z_IRC_NICK
 var irc_channels = process.env.Z_IRC_CHANNELS.split(",")
 
 irc = require("irc");
-irc_connection = irc.Client(irc_server, irc_nick, {
+irc_connection = new irc.Client(irc_server, irc_nick, {
     channels: irc_channels,
+    debug: true,
     floodProtection: true,
     floodProtectionDelay: 333,
     autoConnect: true,
@@ -17,34 +18,59 @@ irc_connection = irc.Client(irc_server, irc_nick, {
 var bot = {'nick': irc_nick, 'irc': irc_connection};
 
 
-// function PrivateMessage(sender, text, raw_message) {
-//     this.sender = sender;
-//     this.text = text;
-//     this.raw = raw_message;
-// }
+// represents a message in a channel
+function InChannelMessage(sender_nick, to, text, raw_message) {
+    this.sender_nick = sender_nick;
+    this.channel = to;
+    this.text = text;
+    this.raw = raw_message;
+    this.bot = bot;
+}
 
-// PrivateMessage.prototype.reply = function(text) {
-//     bot.irc.
-// };
+InChannelMessage.prototype.reply = function(text) {
+    this.bot.irc.say(this.channel, text);
+};
+
+
+// represents a private message
+function PrivateMessage(sender_nick, text, raw_message) {
+    this.sender_nick = sender_nick;
+    this.text = text;
+    this.raw = raw_message;
+    this.bot = bot;
+}
+
+PrivateMessage.prototype.reply = function(text) {
+    this.bot.irc.say(this.sender_nick, text);
+};
 
 
 // set up messageToMe event so not everyone has to reimplement nick-parsing
-bot.irc.addListener('message#', function(sender, to, text, message) {
-    if (message.indexOf(bot.nick + ":") == 0) {
-        bot.irc.emit('messageToMe', sender, text, message);
+bot.irc.addListener('message#', function(nick, to, text, message) {
+    if (text.indexOf(bot.nick + ":") == 0) {
+        bot.irc.emit('messageToMe', new InChannelMessage(nick, to, text, message));
     }
 });
 
-bot.irc.addListener('pm', function(sender, text, message) {
-    bot.irc.emit('messageToMe', sender, text, message);
+bot.irc.addListener('pm', function(nick, text, message) {
+    bot.irc.emit('messageToMe', nick, text, message);
 });
 
-bot.irc.addListener('messageToMe', function(sender, text, message) {
-    echo_index = text.indexOf("echo ");
-    if (echo_index > 0) {
-        bot.irc.send(sender, text.substring(echo_index + "echo ".length,
-                                            text.length));
+
+// Now we have a wide variety of useful (ha!) plugins.
+bot.irc.addListener('messageToMe', function(message) {
+    var echo_prefix = "echo "
+    var echo_index = message.text.indexOf(echo_prefix);
+    if (echo_index >= 0) {
+        message.reply(message.text.substring(echo_index + echo_prefix.length,
+                                             message.text.length));
     }
-        
-    
+});
+
+bot.irc.addListener('messageToMe', function(message) {
+    var index = message.text.indexOf("exterminate yourself");
+    if (index >= 0) {
+        message.reply("EMERGENCY TEMPORAL SHIFT");
+        message.bot.irc.disconnect();
+    }
 });
